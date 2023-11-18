191f3fc312d3b765d457d20be68e237c9b4d2da7
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2023-11-18 14:48:43 +0900

    project init

---
+++ src/main/java/dosopt/server/eqlserver/EqlserverApplication.java
@@ -0,0 +1,13 @@
+package dosopt.server.eqlserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EqlserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EqlserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dosopt/server/eqlserver/EqlserverApplicationTests.java
@@ -0,0 +1,13 @@
+package dosopt.server.eqlserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EqlserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
