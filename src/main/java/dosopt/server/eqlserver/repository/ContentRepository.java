db984509361148865257cfd3788dff247e915826
PgmJun <chltmdwns96@gmail.com>
2023-11-19 21:28:42 +0900

    ✨: feat 컨트롤러 RequestMapping #1

--- src/main/java/dosopt/server/eqlserver/repository/ContentRepository.java
+++ src/main/java/dosopt/server/eqlserver/repository/ContentRepository.java
@@ -1,6 +1,5 @@
 package dosopt.server.eqlserver.repository;
 
-import dosopt.server.eqlserver.domain.Cart;
 import dosopt.server.eqlserver.domain.Content;
 import org.springframework.data.jpa.repository.JpaRepository;
 
--- src/main/java/dosopt/server/eqlserver/repository/ItemRepository.java
+++ src/main/java/dosopt/server/eqlserver/repository/ItemRepository.java
@@ -1,6 +1,5 @@
 package dosopt.server.eqlserver.repository;
 
-import dosopt.server.eqlserver.domain.Content;
 import dosopt.server.eqlserver.domain.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
