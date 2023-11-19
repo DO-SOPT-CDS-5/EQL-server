c64a3b9b4fdb19bcd788046c2d5a185a50b3b607
PgmJun <chltmdwns96@gmail.com>
2023-11-19 21:39:38 +0900

    ✨: feat 장바구니 도메인 객체에 회원, 아이템 연관관계 매핑 #1

--- src/main/java/dosopt/server/eqlserver/domain/Cart.java
+++ src/main/java/dosopt/server/eqlserver/domain/Cart.java
@@ -3,9 +3,12 @@ package dosopt.server.eqlserver.domain;
 import dosopt.server.eqlserver.domain.common.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -20,6 +23,13 @@ public class Cart extends BaseEntity {
     @Column(name = "cart_id")
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id", nullable = false)
+    private Item item;
 
     @Column(name = "item_amount", nullable = false)
     private int amount;
