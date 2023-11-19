b3cc54c5285dd9f7309aa59cc1aa76b508f02ff8
PgmJun <chltmdwns96@gmail.com>
2023-11-19 21:12:38 +0900

    ⚡️: fix WebConfig 패키지 위치 변경 #1

--- src/main/java/dosopt/server/eqlserver/global/config/WebConfig.java
+++ src/main/java/dosopt/server/eqlserver/global/config/WebConfig.java
@@ -1,4 +1,4 @@
-package dosopt.server.eqlserver.config;
+package dosopt.server.eqlserver.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
