370ec8a656e6e3b660057c1e265b0a9ef49abb7f
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2023-11-20 17:31:08 +0900

    ⚡️: fix rebase

--- src/main/java/dosopt/server/eqlserver/repository/ItemRepository.java
+++ src/main/java/dosopt/server/eqlserver/repository/ItemRepository.java
@@ -4,6 +4,4 @@ import dosopt.server.eqlserver.domain.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
-
-
 }
