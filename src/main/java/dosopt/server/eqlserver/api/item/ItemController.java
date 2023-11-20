98edbd3638375bb43dd1e2f36dc17958f2ef8414
PgmJun <chltmdwns96@gmail.com>
2023-11-20 14:30:32 +0900

    ⚡️: fix item, items RequestMapping 유동적으로 사용 #1

--- src/main/java/dosopt/server/eqlserver/api/item/ItemController.java
+++ src/main/java/dosopt/server/eqlserver/api/item/ItemController.java
@@ -3,13 +3,12 @@ package dosopt.server.eqlserver.api.item;
 import dosopt.server.eqlserver.service.item.ItemQueryService;
 import dosopt.server.eqlserver.service.item.ItemService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/item")
 public class ItemController {
     private final ItemService itemService;
     private final ItemQueryService itemQueryService;
+
 }
