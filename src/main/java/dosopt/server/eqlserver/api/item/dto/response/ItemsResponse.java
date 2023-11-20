66dac800925bda23006a9e4ada30c45b9bcc944e
PgmJun <chltmdwns96@gmail.com>
2023-11-20 17:31:19 +0900

    ♻️: refactor ItemResponses 네이밍 변경 #9

--- src/main/java/dosopt/server/eqlserver/api/item/dto/response/ItemsResponse.java
+++ src/main/java/dosopt/server/eqlserver/api/item/dto/response/ItemsResponse.java
@@ -1,12 +1,11 @@
 package dosopt.server.eqlserver.api.item.dto.response;
 
+import java.util.List;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import java.util.List;
-
 @Getter
 @RequiredArgsConstructor
-public class ItemResponses {
+public class ItemsResponse {
     private final List<ItemResponse> itemInfos;
 }
