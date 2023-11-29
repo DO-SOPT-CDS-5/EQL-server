28541852d3d2c84e18b51578acdda961d8445da5
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2023-11-29 13:58:54 +0900

    🚑: hotfix cors에러 해결(5173포트 및 클라이언트 배포 링크 허용)

--- src/main/java/dosopt/server/eqlserver/global/config/WebConfig.java
+++ src/main/java/dosopt/server/eqlserver/global/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:5173", "https://eql-client.vercel.app:5173","http://localhost:8080", "http://localhost:3000", "http://localhost:3001")
                 .allowedMethods("OPTIONS","GET","POST","PUT","DELETE");
     }
 }
